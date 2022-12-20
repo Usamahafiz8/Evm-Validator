fb1abd4fe919cc70c14261ced2c30acd02000182
Usamahafiz8 <Usamahafiz8@gmail.com>
2022-12-20 16:57:10 +0500

    Balance checing Non EVM-Token

--- src/actions/useBalance.js
+++ src/actions/useBalance.js
@@ -4,10 +4,44 @@ import { getERC20Contract } from "../store/contractStore";
 import BigNumber from "bignumber.js";
 import BN from "bn.js";
 import { useWeb3React } from "@web3-react/core";
+import axios from "axios";
+import { GetUSDExchangeRate, GetETHExchangeRate } from "../assets/api";
 
-export default function useBalance(tokenAddress, decimals, checkAccount) {
+export default function useBalance(tokenAddress, decimals, checkAccount, name) {
   // console.log(library);
   const [balance, setBalance] = useState("0");
+  const [usdExRate, setUsdExRate] = useState();
+  // const [ethExRate, setEthExRate] = useState();
+
+  const getElrondBal = async () => {
+    await axios.get(
+      `https://gateway.elrond.com/address/${checkAccount}/balance`
+    ).then(response => {
+      console.log("response", response);
+      const egldBal = response.data.data.balance;
+      console.log("elrond bal", egldBal);
+      setBalance(egldBal);
+    }).catch(err => {
+      console.log(err);
+    })
+  };
+  useEffect(() => {
+    GetUSDExchangeRate().then((res) => {
+      setUsdExRate(parseFloat(res));
+      console.log("usd", parseFloat(res));
+    });
+    // GetETHExchangeRate().then((res) => {
+    //   setEthExRate(parseFloat(res));
+    //   console.log("eth", parseFloat(res));
+    // });
+  }, []);
+
+  useEffect(() => {
+    if (name === "Elrond" && tokenAddress !== ZERO_ADDRESS) {
+      console.log("triggered on change elrond");
+      getElrondBal();
+    }
+  }, [checkAccount, name]);
 
   const { account, library } = useWeb3React();
 
@@ -24,7 +58,7 @@ export default function useBalance(tokenAddress, decimals, checkAccount) {
 
         try {
           if (tokenAddress === ZERO_ADDRESS) {
-            console.log("running zero address with account ", checkAccount);
+            // console.log("running zero address with account ", checkAccount);
 
             library.eth
               .getBalance(checkAccount)
@@ -64,7 +98,8 @@ export default function useBalance(tokenAddress, decimals, checkAccount) {
       if (!isCancelled) {
         const pow = new BigNumber("10").pow(new BigNumber(decimals));
         const b = web3BNToFloatString(bn, pow, 4, BigNumber.ROUND_DOWN);
-        const dollarBalance = b * 1214;
+        // console.log("eth value", usdExRate);
+        const dollarBalance = b * usdExRate;
         setBalance(dollarBalance);
       }
     }
@@ -74,7 +109,7 @@ export default function useBalance(tokenAddress, decimals, checkAccount) {
     return () => {
       isCancelled = true;
     };
-  }, [tokenAddress, library, decimals, account, checkAccount]);
+  }, [tokenAddress, library, decimals, account, checkAccount, usdExRate]);
   // console.log("library", library);
   // console.log("decimal", decimals);
   // console.log("account", account);
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import TokenList from "../assets/token-list.json";
 import Wallets from "../assets/wallet-address.json";
 import { useEffect, useState } from "react";
 import useBalance from "../actions/useBalance";
-import axios from 'axios';
 
 export default function Home() {
   const [selectedToken, setSelectedToken] = useState(TokenList[0]);
@@ -18,22 +17,19 @@ export default function Home() {
     setCheckAccount(selectedWallet.address);
   }, [selectedWallet]);
 
-  const getElrondBal = async () => {
-    const api = 'https://gateway.elrond.com/address/:bech32Address/balance'
-    const response = await axios.get(`https://gateway.elrond.com/address/${checkAccount}/balance`)
-    .then(response => {
-      const bal = response.data.balance;
-      console.log("balllll", bal);
+  
+  // useEffect(()=>{
+  //   const getElrondBal = async () => {
+  //     // const api = 'https://gateway.elrond.com/address/:bech32Address/balance'
+  //     const response = await axios.get(`https://gateway.elrond.com/address/${checkAccount}/balance`)
       
-    })
-    .catch(err => console.log(err));
-  }
-
-  useEffect(()=>{
-    if (selectedToken.name === 'Elrond'){
-      getElrondBal();
-    }
-    },[selectedToken])
+  //     console.log("response", response);
+  //   }
+  //   if (selectedToken.name === 'Elrond'){
+  //     console.log("triggered on change elrond");
+  //     getElrondBal();
+  //   }
+  // },[selectedToken, selectedWallet])
 
   // const accountHandler = (e) => {};
   const submitHandler = (e) => {
@@ -43,12 +39,14 @@ export default function Home() {
   const [balance] = useBalance(
     selectedToken.address,
     selectedToken.decimals,
-    checkAccount
+    checkAccount,
+    selectedToken.name
   );
   console.log("balance ", balance);
-  console.log("checkAccount ", checkAccount);
-  console.log("selectToken", selectedToken);
-  console.log("selectWallet", selectedWallet);
+  // console.log("checkAccount ", checkAccount);
+  // console.log("selectToken", selectedToken);
+  // console.log("selectWallet", selectedWallet);
+  // console.log("selectWallet", selectedWallet);
 
   return (
     <div className={styles.container}>
